fa8f47f14e1c55642a0661bfc852fd1bb502730c
AHMED Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-02-28 21:55:19 +0200

    ios

--- lib/core/constant/applink.dart
+++ lib/core/constant/applink.dart
@@ -1,7 +1,9 @@
 class AppLink {
-  static String server = "https://amadytech.com/my_portfolio/user";
+  // static String server = "https://amadytech.com/my_portfolio/user";
+  static String server = "https://ahmed-eid.c1.biz/backend/user";
   static String test = "$server/test.php";
-  static String image = "https://amadytech.com/my_portfolio/upload";
+  // static String image = "https://amadytech.com/my_portfolio/upload";
+  static String image = "https://ahmed-eid.c1.biz/backend/upload";
   static String imagePL = "$image/projects_list";
   static String imageServices = "$image/services";
 
